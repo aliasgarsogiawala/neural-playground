af89b0f9f94f15b5da73cc30b88f59123ffa116d
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2026-05-27 03:57:45 +0530

    Add ResearcgResponse model and import necessary classes for output parsing

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
 
+class ResearcgResponse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used:list[str]
+
 llm= ChatOpenAI(model='gpt-4o-mini')
-response=llm.invoke("What is the meaning of life?")
-print(response)
