dd7458321103a3b60c7b28c57a640b5b08349544
Aliasgar Sogiawala <vertexxinfotech18@gmail.com>
2026-05-27 04:04:56 +0530

    Refactor ResearchResponse to include prompt template for research paper generation

--- main.py
+++ main.py
@@ -13,7 +13,21 @@ class ResearchResponse(BaseModel):
     sources: list[str]
     tools_used:list[str]
 
-
-
 llm= ChatOpenAI(model='gpt-4o-mini')
 parser=PydanticOutputParser(pydantic_object=ResearchResponse)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            """
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
+            """,
+        ),
+        ("placeholder", "{chat_history}"),
+        ("human", "{query}"),
+        ("placeholder", "{agent_scratchpad}"),
+    ]
+).partial(format_instructions=parser.get_format_instructions())
